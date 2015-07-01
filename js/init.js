d0f05cb174410627ead120fbaed5d81b5bb7a744
Jeremy Ramin <ebneyaminj@gmail.com>
2015-07-01 00:39:22 -0700

    Moved contact and about pages into root

--- js/init.js
+++ js/init.js
@@ -10,6 +10,8 @@
     $('.slider').slider({full_width: true});
     
     $('.collapsible').collapsible();
+    
+    $('select').material_select();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
