17ab7b9977450f0124b92f8106abe56fce771c7b
Jeremy Ramin <ebneyaminj@gmail.com>
2015-08-01 23:51:48 -0700

    Filter webshims from filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -242,6 +242,7 @@ module.exports = function (grunt) {
         files: [{
           src: [
             '<%= yeoman.dist %>/js/**/*.js',
+            '!<%= yeoman.dist %>/js/shims/**/*',
             '<%= yeoman.dist %>/css/**/*.css',
             '<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
             '<%= yeoman.dist %>/fonts/**/*.{eot*,otf,svg,ttf,woff}'
